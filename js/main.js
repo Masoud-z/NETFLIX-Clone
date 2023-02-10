3e124b2153e9671f9a248ae4dd2e2f79cb9c341a
Masoud <masoud.zeinalabedini@gmail.com>
2023-02-10 13:25:50 +0330

    Update Footer

--- js/main.js
+++ js/main.js
@@ -6,38 +6,38 @@ const langBtnTitle = document.getElementById("langBtnTitle");
 const langList = document.getElementById("langList");
 const eng = document.getElementById("eng");
 const esp = document.getElementById("esp");
-const inputText = document.getElementById("inputText");
-const emailInput = document.getElementById("emailInput");
 
-body.addEventListener("wheel", function (e) {
+body.addEventListener("wheel", function () {
   langList.classList.add("hide-list");
 });
 
-body.addEventListener("click", function (e) {
-  if (
-    e.target.id != "langBtn" &&
-    e.target.id != "langBtnTitle" &&
-    e.target.id != "langList" &&
-    e.target.id != "eng" &&
-    e.target.id != "esp"
-  ) {
+body.addEventListener("click", function () {
     langList.classList.add("hide-list");
-  }
 });
 
-langBtn.addEventListener("click", function () {
+langBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
   langList.classList.toggle("hide-list");
 });
 
-eng.addEventListener("click", function () {
+eng.addEventListener("click", function (e) {
+  e.stopPropagation();
   langBtnTitle.innerHTML = "English";
   langList.classList.add("hide-list");
 });
 
-esp.addEventListener("click", function () {
+esp.addEventListener("click", function (e) {
+  e.stopPropagation();
   langBtnTitle.innerHTML = "Español";
   langList.classList.add("hide-list");
 });
+
+//---------------++++---------------
+
+//---------------Change input text position on focus---------------
+
+const inputText = document.getElementById("inputText");
+const emailInput = document.getElementById("emailInput");
 
 emailInput.addEventListener("focus", function () {
   inputText.classList.add("move-text");
@@ -48,6 +48,22 @@ emailInput.addEventListener("blur", function () {
 });
 
 //---------------++++---------------
+
+//---------------Change FadnQ input text position on focus---------------
+
+const FQinputText = document.getElementById("FQinputText");
+const FQemailInput = document.getElementById("FQemailInput");
+
+FQemailInput.addEventListener("focus", function () {
+  FQinputText.classList.add("move-text");
+});
+
+FQemailInput.addEventListener("blur", function () {
+  FQinputText.classList.remove("move-text");
+});
+
+//---------------++++---------------
+
 
 //---------------Questions---------------
 
@@ -66,6 +82,44 @@ questions.forEach((question) => {
     const targetQuestion = e.currentTarget.parentElement;
     targetQuestion.classList.toggle("showAnswer");
   });
+});
+
+//---------------++++---------------
+
+
+
+//---------------Change footer language button---------------
+
+const flangBtn = document.getElementById("flangBtn");
+const flangBtnTitle = document.getElementById("flangBtnTitle");
+const flangList = document.getElementById("flangList");
+const feng = document.getElementById("feng");
+const fesp = document.getElementById("fesp");
+
+body.addEventListener("wheel", function () {
+  flangList.classList.add("hide-list");
+});
+
+body.addEventListener("click", function () {
+    flangList.classList.add("hide-list");
+});
+
+flangBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  e.preventDefault();
+  flangList.classList.toggle("hide-list");
+});
+
+feng.addEventListener("click", function (e) {
+  e.stopPropagation();
+  flangBtnTitle.innerHTML = "English";
+  flangList.classList.add("hide-list");
+});
+
+fesp.addEventListener("click", function (e) {
+  e.stopPropagation();
+  flangBtnTitle.innerHTML = "Español";
+  flangList.classList.add("hide-list");
 });
 
 //---------------++++---------------
